0fcb351b4067a5e8eaa64a3f552edfec6eccc37c
rainbow1223 <greatwisdom1223@gmail.com>
2023-07-09 02:04:29 -0700

    add routes to the individual blogs with Link

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head';
+import Link from 'next/link';
+// Import Components
 import Layout, { siteTitle } from '../components/layout';
+import Date from '../components/date';
+// Import CSS files
 import utilStyles from '../styles/utils.module.css';
-
-
 // import { getSortedPostsData } from '../lib/posts';
 import { getSortedPostsData } from '../lib/posts';
 
@@ -33,12 +35,12 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
+            <Link href={`/posts/${id}`}>{title}</Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
           ))}
         </ul>
       </section>
