c4233dcbb29715f5f7df97d0c41022c4dfb700ce
rainbow1223 <greatwisdom1223@gmail.com>
2023-07-09 01:05:51 -0700

    make change on index.js

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,9 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am a senior Next.js developer</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you’ll be building a site like this on{' '})
         </p>
       </section>
 
